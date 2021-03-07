60547cc7147eadfd32501bbfb8cd35868bbd5dfc
Krzysztof Szymkiewicz <Krzysztofsz81@gmail.com>
2021-03-07 20:23:56 +0100

    fix: add secrets example file

--- include/secrets.h
+++ include/secrets.h
@@ -1,4 +1,5 @@
-#define WIFI_SSID "SSID"
-#define WIFI_PASS "PASS"
-#define WS_HOST "192.168.1.1"
+#define WIFI_SSID "Szymborska"
+#define WIFI_PASS "Rybekpsotek81"
+// #define WS_HOST "192.168.1.181"
+#define WS_HOST "192.168.1.11"
 #define WS_PORT 7000
