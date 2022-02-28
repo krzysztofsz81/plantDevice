b1a02a2704d99fafe57c5e9c76d7ea3198da441e
Krzysztof Szymkiewicz <Krzysztofsz81@gmail.com>
2022-02-28 20:07:47 +0100

    feat: clear before final version

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <Preferences.h>
+
 #include <secrets.h>
 
 #include <WiFi.h>
@@ -6,7 +8,7 @@
 #include <ArduinoJson.h>
 #include <WebSocketsClient.h>
 
-// dht sensor lib
+// DHT21 sensor lib
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
@@ -15,6 +17,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+
 // pins
 #define DHT_PIN 4
 #define SOIL_MOISTURE_PIN A0
@@ -22,6 +25,8 @@
 #define WATER_PUMP_PIN 13
 
 #define WIFI_TIMEOUT 1000
+
+Preferences preferences;
 
 OneWire oneWire(SOIL_TEMPERATURE_PIN);
 DallasTemperature sensors(&oneWire);
@@ -33,7 +38,15 @@ String action_id;
 String action_periphal;
 String action_state;
 
-int waterPumpTime = 2000;
+String waterPumpDef = "water_pump:BUTTON/";
+String waterPumpTimeDef = "water_pump_time:RANGE(1000-10000)/";
+String soilMoistureDef = "soil_moisture:RANGE(0-4095)/";
+String soilTemperatureDef = "soil_temperature:CELSIUS/";
+String dhtHumidityDef = "dht_humidity:PERCENT/";
+String dhtTemperatureDef = "dht_temperature:CELSIUS/";
+
+int waterPumpTime = preferences.getUInt("time", 2000);
+int rangedWaterPumpTime;
 
 String getValueFromPayload(String data, char separator, int index)
 {
@@ -56,124 +69,72 @@ String getValueFromPayload(String data, char separator, int index)
 float getDHTTemperature() {
     sensors_event_t event;
     dht.temperature().getEvent(&event);
-    // if (isnan(event.temperature)) {
-    //     Serial.println(F("Error reading temperature!"));
-    // } else {
-    //     Serial.print(F("Temperature: "));
-    //     Serial.print(event.temperature);
-    //     Serial.println(F("°C"));
-    // }
     return event.temperature;
 }
 
 float getDHTHumidity() {
     sensors_event_t event;
     dht.humidity().getEvent(&event);
-    // if (isnan(event.relative_humidity)) {
-    //     Serial.println(F("Error reading humidity!"));
-    // } else {
-    //     Serial.print(F("Humidity: "));
-    //     Serial.print(event.relative_humidity);
-    //     Serial.println(F("%"));
-    // }
     return event.relative_humidity;
 }
 
 float getSoilTemperature() {
     sensors.requestTemperatures();
+    delay(1000);
     return sensors.getTempCByIndex(0);
 }
 
 void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
-    Serial.print("webSocketEvent type: ");
-    Serial.println(type); 
     switch (type) {
-        case WStype_ERROR:
-            Serial.println("[WS] WStype_ERROR");
-            break;
-        case WStype_DISCONNECTED:
-            Serial.println("[WS] WStype_DISCONNECTED");
-            break;
         case WStype_CONNECTED:
-            Serial.printf("[WS] WStype_CONNECTED: %s\n", payload);
-            webSocket.sendTXT("REGISTER_INPUT/water_pump:Boolean/false");
-            webSocket.sendTXT("REGISTER_INPUT/water_pump_time:Int/2000");
-            webSocket.sendTXT("REGISTER_OUTPUT/soil_moisture:Int:Percent:true/" + String(analogRead(SOIL_MOISTURE_PIN)));
-            webSocket.sendTXT("REGISTER_OUTPUT/soil_temperature:Float:Celsius/" + String(getSoilTemperature()));
-            webSocket.sendTXT("REGISTER_OUTPUT/dht_humidity:Float:Percent/" + String(getDHTTemperature()));
-            webSocket.sendTXT("REGISTER_OUTPUT/dht_temperature:Float:Celsius/" + String(getDHTHumidity()));
+            webSocket.sendTXT("REGISTER_INPUT/" + waterPumpDef + "false");            
+            webSocket.sendTXT("REGISTER_INPUT/" + waterPumpTimeDef + String(waterPumpTime));
+            webSocket.sendTXT("REGISTER_OUTPUT/" + soilMoistureDef + String(4095 - analogRead(SOIL_MOISTURE_PIN)));
+            webSocket.sendTXT("REGISTER_OUTPUT/" + soilTemperatureDef + String(getSoilTemperature()));
+            webSocket.sendTXT("REGISTER_OUTPUT/" + dhtHumidityDef + String(getDHTHumidity()));
+            webSocket.sendTXT("REGISTER_OUTPUT/" + dhtTemperatureDef + String(getDHTTemperature()));
             break;
         case WStype_TEXT:
-            Serial.printf("[WS] WStype_TEXT: %s\n", payload);
             action_id = getValueFromPayload((char *)payload, '/', 0);
             if (action_id == "RETRIEVE_PERIPHAL_DATA") {
-                webSocket.sendTXT("UPDATE/soil_moisture:Int:Percent:true/" + String(analogRead(SOIL_MOISTURE_PIN)));
-                webSocket.sendTXT("UPDATE/soil_temperature:Float:Celsius/" + String(getSoilTemperature()));
-                webSocket.sendTXT("UPDATE/dht_humidity:Float:Percent/" + String(getDHTTemperature()));
-                webSocket.sendTXT("UPDATE/dht_temperature:Float:Celsius/" + String(getDHTHumidity()));
+                webSocket.sendTXT("UPDATE/" + soilMoistureDef + String(4095 - analogRead(SOIL_MOISTURE_PIN)));
+                webSocket.sendTXT("UPDATE/" + soilTemperatureDef + String(getSoilTemperature()));
+                webSocket.sendTXT("UPDATE/" + dhtHumidityDef + String(getDHTHumidity()));
+                webSocket.sendTXT("UPDATE/" + dhtTemperatureDef + String(getDHTTemperature()));
             }
             if (action_id == "SET_PERIPHAL_DATA") {
                 action_periphal = getValueFromPayload((char *)payload, '/', 1);
                 action_state = getValueFromPayload((char *)payload, '/', 2);
                 if (action_periphal == "water_pump_time") {
-                    Serial.println("[WATER PUMP] Set time: " + String(action_state.toInt()));
                     waterPumpTime = action_state.toInt();
+                    preferences.putUInt("time", waterPumpTime);
                 }
                 if (action_periphal == "water_pump") {
                     if (action_state == "true") {
-                        Serial.println("[WATER PUMP] Enabled");
                         digitalWrite(WATER_PUMP_PIN, LOW);
-
-                        Serial.print("[WATER PUMP] Delay: ");
-                        Serial.println(waterPumpTime);
-                        delay(waterPumpTime);
+                        if (waterPumpTime < 1000) rangedWaterPumpTime = 1000;
+                        if (waterPumpTime > 10000) rangedWaterPumpTime = 10000;
+                        if (waterPumpTime >= 1000 && waterPumpTime <= 10000) rangedWaterPumpTime = waterPumpTime;
+                        delay(rangedWaterPumpTime);
                         digitalWrite(WATER_PUMP_PIN, HIGH);
-                        webSocket.sendTXT("UPDATE/water_pump:Boolean/false");
+                        webSocket.sendTXT("UPDATE/" + waterPumpDef + "false");
                     } else {
-                        Serial.println("[WATER PUMP] Disabled");
                         digitalWrite(WATER_PUMP_PIN, HIGH);
                     }
                 }
             }
-            break;
-        case WStype_BIN:
-            Serial.println("[WS] WStype_BIN");
-            break;
-        case WStype_FRAGMENT_TEXT_START:
-            Serial.println("[WS] WStype_FRAGMENT_TEXT_START");
-            break;
-        case WStype_FRAGMENT_BIN_START:
-            Serial.println("[WS] WStype_FRAGMENT_BIN_START");
-            break;
-        case WStype_FRAGMENT:
-            Serial.println("[WS] WStype_FRAGMENT");
-            break;
-        case WStype_FRAGMENT_FIN:
-            Serial.println("[WS] WStype_FRAGMENT_FIN");
-            break;
-        case WStype_PING:
-            Serial.println("[WS] WStype_PING");
-            break;
-        case WStype_PONG:
-            Serial.println("[WS] WStype_PONG");
             break;
     }
 }
 
 void connectToWiFi() {
     WIFI.addAP(WIFI_SSID, WIFI_PASS);
-    while (WIFI.run() != WL_CONNECTED) {
-        Serial.println("[WIFI] Connecting ...");
-         delay(WIFI_TIMEOUT);
-    }
-    Serial.println("[WIFI] Connected");
+    while (WIFI.run() != WL_CONNECTED) delay(WIFI_TIMEOUT);
 }
 
 void initWebSocket() {
-    Serial.println("[WebSocket] Connecting ...");
     webSocket.begin(WS_HOST, WS_PORT, "/?deviceId=plant_1");
     webSocket.onEvent(webSocketEvent);
-    // webSocket.setAuthorization("user", "Password");
     webSocket.setReconnectInterval(5000);
 }
 
@@ -185,8 +146,10 @@ void setup() {
     pinMode(SOIL_TEMPERATURE_PIN, INPUT);
     pinMode(DHT_PIN, INPUT);
 
+    preferences.begin("plantDevice", false); 
     dht.begin();
     sensors.begin();
+    sensors.requestTemperatures();
 
     Serial.begin(115200);
     Serial.setDebugOutput(true);
